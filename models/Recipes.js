989812effcac182f40a72202239651e51677fbd2
Phat Doan <phatdoan@yahoo.com>
2019-01-31 13:31:04 -0500

    delete and add new file

--- models/Recipes.js
+++ models/Recipes.js
@@ -1,6 +1,6 @@
-const mongoose = require("../db/connection");
+const mongoose = require("../db/connection")
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 // define a schema where the field-name is the key in the object passed in
 // as an argument to mongoose.Schema, and the object values are the data type of that field
@@ -9,13 +9,13 @@ const Recipe = new Schema({
   title: String,
   ingredients: String,
   instructions: String
-});
+})
 
-module.exports = mongoose.model("Recipe", Recipe);
+module.exports = mongoose.model("Recipe", Recipe)
 
 // builds a model from the schema, and attaches it to our mongoose instance.
 // a model is used to query and change data in the database
 //Double schemas into one Model File, Export both.
 // module.exports = {
 //   Recipes: mongoose.model("Recipes", Recipes),
-//   Comments: mongoose.model("Comments", Comments) }
+//   Comments: mongoose.model("Comments", Comments)
