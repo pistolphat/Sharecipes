0f710811e4f1e77dad8e23ab1ed4423c81d60d81
Phat Doan <phatdoan@yahoo.com>
2019-01-31 13:22:16 -0500

    testing

--- models/recipes.js
+++ models/recipes.js
@@ -8,18 +8,14 @@ const Schema = mongoose.Schema;
 const Recipe = new Schema({
   title: String,
   ingredients: String,
-  instructions: String,
+  instructions: String
 });
 
-// const Recipe = mongoose.model('Recipe', Recipe)
-
-//export the model
-module.exports = mongoose.model('Recipe', Recipe);
+module.exports = mongoose.model("Recipe", Recipe);
 
 // builds a model from the schema, and attaches it to our mongoose instance.
 // a model is used to query and change data in the database
 //Double schemas into one Model File, Export both.
 // module.exports = {
 //   Recipes: mongoose.model("Recipes", Recipes),
-//   Comments: mongoose.model("Comments", Comments)
-// };
+//   Comments: mongoose.model("Comments", Comments) }
